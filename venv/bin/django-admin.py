65f7b404f07b64630a26f9bd1aa0342f7fda6d68
PHILIPP RICHTER <philipp.richter@gmail.com>
2013-06-18 16:16:12 +0200

    removed venv and .project from repo

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/philipprichter/Documents/myrunningorder_workspace/mro/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
