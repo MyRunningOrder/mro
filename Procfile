577627a3c6ef80afb19107916ef5aa0fc4c20387
PHILIPP RICHTER <philipp.richter@gmail.com>
2013-06-18 15:44:31 +0200

    added venv and Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MyRunningOrder.wsgi
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/philipprichter/Documents/myrunningorder_workspace/mro/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
