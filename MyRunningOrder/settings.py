465d0e59d0858488f0dcae07cce7e60cb02211c2
PHILIPP RICHTER <philipp.richter@gmail.com>
2013-06-21 16:09:33 +0200

    added support for static files and locally enabled databases

--- MyRunningOrder/settings.py
+++ MyRunningOrder/settings.py
@@ -14,10 +14,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'MyRunningOrder_DEV',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'MyRunningOrder',
-        'PASSWORD': 'ficken',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+#        'USER': 'MyRunningOrder',
+#        'PASSWORD': 'ficken',
+#        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
@@ -61,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -159,7 +159,7 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default='postgres://myrunningorder:ficken@localhost:5432/myrunningorder_dev')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- MyRunningOrder/urls.py
+++ MyRunningOrder/urls.py
@@ -8,11 +8,15 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'MyRunningOrder.views.home', name='home'),
     # url(r'^MyRunningOrder/', include('MyRunningOrder.foo.urls')),
+    
+    url(r'^$', include('choosesomeacts.urls')),
+    url(r'^choosesomeacts/', include('choosesomeacts.urls')),
+
+    url(r'^static/(.*)$', 'django.views.static.serve', {'document_root':'static/'}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^choosesomeacts/', include('choosesomeacts.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
