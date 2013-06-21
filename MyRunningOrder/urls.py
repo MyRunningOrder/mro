6f88d5181cb9ab8213d715e3693a506b90cdcd4e
PHILIPP RICHTER <philipp.richter@gmail.com>
2013-06-21 16:43:11 +0200

    changed root url to point to choosesomeacts/w

--- MyRunningOrder/urls.py
+++ MyRunningOrder/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from django.http import HttpResponseRedirect
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -8,9 +10,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'MyRunningOrder.views.home', name='home'),
     # url(r'^MyRunningOrder/', include('MyRunningOrder.foo.urls')),
-    
+   
+    url(r'^$', lambda r : HttpResponseRedirect('choosesomeacts/')),
     url(r'^choosesomeacts/', include('choosesomeacts.urls')),
-
     url(r'^static/(.*)$', 'django.views.static.serve', {'document_root':'static/'}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
